bfc071900dc4c07e412902ef83d0a4dc1b908ecb
dmitryja24 <dmitryja@gmx.com>
2019-01-31 22:36:58 +0200

    commit 2

---
+++ Calculator.java
@@ -0,0 +1,20 @@
+
+public class Calculator {
+
+    private int result;
+
+    public void add (int ... params) {
+        for (int param : params){
+            this.result += param;
+        }
+    }
+
+    public int getResult(){
+        return this.result;
+    }
+
+    public void clearResult() {
+        this.result = 0;
+    }
+
+}
